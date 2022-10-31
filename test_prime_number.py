f91bf56553b1dcebe586d884fac47d0ec162455e
Out-A-Time <101604946+Out-A-Time@users.noreply.github.com>
2022-10-31 09:19:44 +0000

    Simple tests for checking is input a prime number

---
+++ test_prime_number.py
@@ -0,0 +1,22 @@
+import unittest
+from prime_number import is_prime
+
+# Four test methods, each checking for a specific input
+
+
+class TestPrime(unittest.TestCase):
+    def test_two(self):
+        self.assertTrue(is_prime(2))
+
+    def test_five(self):
+        self.assertTrue(is_prime(5))
+
+    def test_nine(self):
+        self.assertFalse(is_prime(9))
+
+    def test_eleven(self):
+        self.assertTrue(is_prime(11))
+
+
+if __name__ == '__main__':
+    unittest.main()
